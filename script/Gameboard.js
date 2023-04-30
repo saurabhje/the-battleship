1a124022a7494b3ef094a9307d1dd2e912b90a4c
saurabhje <work.saurabh@outlook.com>
2023-04-30 21:47:43 +0530

    Gameboard factory

---
+++ script/Gameboard.js
@@ -0,0 +1,36 @@
+const Ship = require('./ship');
+
+function GameboardFactory() {
+  let board = Array(100).fill(null);
+  let ships = [];
+  let missedAttacks = [];
+
+  function placeShip(ship, coordinates) {
+    coordinates.forEach(coord => {
+      board[coord] = ship;
+    });
+    ships.push(ship);
+  }
+
+  function receiveAttack(coordinates) {
+    if (board[coordinates] instanceof Ship) {
+      let hitShip = board[coordinates];
+      hitShip.hit();
+      return true;
+    } else {
+      missedAttacks.push(coordinates)
+      return false;
+    }
+  }
+
+  function allShipsSunk() {
+    return ships.every(ship => ship.isSunk());
+  }
+
+  return {
+    placeShip,
+    receiveAttack,
+    allShipsSunk,
+    missedAttacks,
+  };
+}
