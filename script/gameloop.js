ff7ce09c53b9387f2fa4ed3fd0f161daf1d801ad
saurabhje <work.saurabh@outlook.com>
2023-05-20 20:49:06 +0530

    Fixed shipPlace by Player

--- script/gameloop.js
+++ script/gameloop.js
@@ -1,6 +1,4 @@
-import Players from "./player";
 import Gameboard from "./gameboard";
-import Ship from "./ship";
 import doM from "./DOM";
 import { placeships } from "./placeships";
 
@@ -11,37 +9,43 @@ function gameLoop(player1, player2) {
   const clickedCells = [];
   const player1board = document.getElementById("playerboard");
   const player2board = document.getElementById("compboard");
-  doM.renderBoard(player1board,"player");
+  doM.renderBoard(player1board, "player");
   doM.renderBoard(player2board, "computer");
 
 
-  placeships("player",playerboard);
-  placeships("computer",compboard);
+  placeships("computer", compboard);
 
- console.log(compboard.getShips())
+  let playerShips = playerboard.getShips();
+  console.log(compboard.getShips());
   let previousAttack = new Set(); //to prevent the comp from attacking the same index twice
   let currentPlayer = player1;
   let gameOver = false;
-  let previousHits = [];
+  let left = null;
+  let right = null;
 
-  
+  console.log(playerShips);
+  let shipPlaced = false;
+  shipPlaced =   placeships("player", playerboard);
   player2board.addEventListener("click", (e) => {
-    const isvalidclick = checkDouble(e);
-    if(isvalidclick){
-      if (currentPlayer !== player1 || gameOver) {
-        return;
+      if(shipPlaced){
+        const isvalidclick = checkDouble(e);
+        if (isvalidclick) {
+          if (currentPlayer !== player1 || gameOver) {
+            return;
+          }
+          handleClick(e);
+          checkgameOver();
+          currentPlayer = switchPlayer();
+          setTimeout(computerTurn, 100);
+        }
       }
-      handleClick(e);
-      checkgameOver();
-      currentPlayer = switchPlayer();
-      setTimeout(computerTurn, 100);
-    }
-    
-  });
-  
-  function checkDouble(e){
+
+    });
+
+
+  function checkDouble(e) {
     const cellindex = e.target.dataset.index;
-    if(clickedCells.includes(cellindex)){
+    if (clickedCells.includes(cellindex)) {
       return false;
     }
     clickedCells.push(cellindex);
@@ -60,43 +64,37 @@ function gameLoop(player1, player2) {
       event.target.style.backgroundColor = "#02d91b";
     }
   }
+
   let attackIndex;
 
   function computerTurn() {
-    if(previousHits.length > 0){
-      const len = previousHits.length
-      const lastAttack = previousHits[len - 1];
-      const lastAdjacentIndex = getAdjacentIndex(lastAttack);
-      console.log(lastAdjacentIndex);
-      const leftIndex = lastAdjacentIndex[0];
-      console.log(`left- ${leftIndex}`);
-      const rightIndex = lastAdjacentIndex[1];
-      console.log(`right - ${rightIndex}`);
-        if(!previousAttack.has(leftIndex)){
-          attackIndex = leftIndex;
-          console.log(`1: ${attackIndex}`);
-        }
-        if(!previousAttack.has(rightIndex) && lastAttack !== leftIndex && previousAttack.has(leftIndex)){
-          attackIndex = rightIndex;
-          console.log(`2: ${attackIndex}`);
-        }
-        previousHits.pop();
-      } else{
-      attackIndex = randomAttack();
+    if (left !== null || right !== null) {
+      if (left !== null) {
+        attackIndex = left;
+        left = left - 1;
+      } else if (right !== null) {
+        attackIndex = right;
+        right = right + 1;
       }
-    console.log( ` real attack - ${attackIndex}`);
+    } else {
+      attackIndex = randomAttack();
+    }
+
     computerAI(attackIndex);
   }
 
-  function randomAttack(){
+  function randomAttack() {
     let x = Math.floor(Math.random() * 100);
-    while(previousAttack.has(x)){
+    while (previousAttack.has(x)) {
       x = Math.floor(Math.random() * 100);
+    }
+    if (previousAttack.has(x)) {
+      randomAttack();
     }
     return x;
   }
-  
-  function computerAI(i){
+
+  function computerAI(i) {
     if (gameOver) {
       return;
     }
@@ -104,28 +102,29 @@ function gameLoop(player1, player2) {
     const cell = player1board.querySelector(`[data-index='${i}']`);
     if (comphit === true) {
       cell.style.backgroundColor = "#f70202";
-      previousHits.push(i);
-      console.log(previousHits);
+      if (left === null) {
+        left = i - 1;
+      }
+      if (previousAttack.has(left)) {
+        left = null;
+      }
+      if (right === null) {
+        right = i + 1;
+      }
+      if (previousAttack.has(right)) {
+        right = null;
+      }
     } else {
       cell.style.backgroundColor = "#02d91b";
+      if (left !== null)
+        left = null;
+      else
+        right = null;
     }
     previousAttack.add(i);
     checkgameOver();
     currentPlayer = switchPlayer();
   }
-
-  function getAdjacentIndex(index){
-    const adjacentIndexes = [];
-    const col = index % 10;
-    if(col > 0){
-      adjacentIndexes.push(index - 1);
-    }
-    if(col < 9){
-      adjacentIndexes.push(index + 1);
-    }
-    return adjacentIndexes;
-  }
-
 
   function switchPlayer() {
     return (currentPlayer = currentPlayer === player1 ? player2 : player1);
